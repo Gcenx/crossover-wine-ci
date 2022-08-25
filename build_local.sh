a3528326b2776d844cbedcc28eb0c4c794c9ccf8
Dean M Greer <38226388+Gcenx@users.noreply.github.com>
2022-08-25 13:18:17 -0400

    build_local.sh: Tweak my liking

--- build_local.sh
+++ build_local.sh
@@ -7,26 +7,30 @@ echo Wine-Crossover-MacOS
 export GITHUB_WORKSPACE=$(pwd)
 
 if [ -z "$CROSS_OVER_VERSION" ]; then
-    export CROSS_OVER_VERSION=21.2.0
+    export CROSS_OVER_VERSION=22.0.0
     echo "CROSS_OVER_VERSION not set building crossover-wine-${CROSS_OVER_VERSION}"
 fi
 
 # avoid weird linker errors with Xcode 10 and later
 export MACOSX_DEPLOYMENT_TARGET=10.14
+
 # crossover source code to be downloaded
 export CROSS_OVER_SOURCE_URL=https://media.codeweavers.com/pub/crossover/source/crossover-sources-${CROSS_OVER_VERSION}.tar.gz
 export CROSS_OVER_LOCAL_FILE=crossover-${CROSS_OVER_VERSION}
+
 # directories / files inside the downloaded tar file directory structure
 export WINE_CONFIGURE=$GITHUB_WORKSPACE/sources/wine/configure
 export DXVK_BUILDSCRIPT=$GITHUB_WORKSPACE/sources/dxvk/package-release.sh
+
 # build directories
 export BUILDROOT=$GITHUB_WORKSPACE/build
+
 # target directory for installation
 export INSTALLROOT=$GITHUB_WORKSPACE/install
 export PACKAGE_UPLOAD=$GITHUB_WORKSPACE/upload
+
 # artifact names
 export WINE_INSTALLATION=wine-cx${CROSS_OVER_VERSION}
-export DXVK_INSTALLATION=dxvk-cx${CROSS_OVER_VERSION}
 
 # Need to ensure Instel brew actually exists
 if ! command -v "/usr/local/bin/brew" &> /dev/null
@@ -39,35 +43,26 @@ fi
 # Manually configure $PATH
 export PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Library/Apple/usr/bin"
 
-echo Getting latest HomeBrew formulas/bottles
+echo Updating brew
 brew update
 
 echo Installing Dependencies
 # build dependencies
 brew install   bison                \
-               cmake                \
                gcenx/wine/cx-llvm   \
-               mingw-w64            \
-               ninja
+               mingw-w64
 
 # runtime dependencies for crossover-wine
-brew install   faudio               \
-               freetype             \
+brew install   freetype             \
                gnutls               \
                gphoto2              \
                gst-plugins-base     \
-               libpng               \
-               little-cms2          \
                molten-vk            \
-               mpg123               \
                sane-backends        \
                sdl2
 
-echo "Add bison to PATH"
-export PATH="$(brew --prefix bison)/bin":${PATH}
-
-echo "Add cx-llvm to PATH"
-export PATH="$(brew --prefix cx-llvm)/bin":${PATH}
+echo "Add cx-llvm & bison to PATH"
+export PATH= "$(brew --prefix cx-llvm)/bin":"$(brew --prefix bison)/bin":${PATH}
 
 
 ############ Download and Prepare Source Code ##############
@@ -104,42 +99,15 @@ if [[ ${CROSS_OVER_VERSION} == 20.* ]]; then
 fi
 
 
-############ Build DXVK ##############
-
-if [[ ${CROSS_OVER_VERSION} == 21.* ]]; then
-    if [[ ! -f "${PACKAGE_UPLOAD}/${DXVK_INSTALLATION}.tar.gz" ]]; then
-        echo "Applying patches to DXVK"
-        pushd sources/dxvk
-        patch -p1 < ${GITHUB_WORKSPACE}/0001-build-macOS-Fix-up-for-macOS.patch
-        patch -p1 < ${GITHUB_WORKSPACE}/0002-fix-d3d11-header-for-MinGW-9-1883.patch
-        patch -p1 < ${GITHUB_WORKSPACE}/0003-fixes-for-mingw-gcc-12.patch
-        popd
-
-        echo "Installing dependencies for DXVK"
-        brew install  meson     \
-                      glslang
-
-        echo "Build DXVK"
-        ${DXVK_BUILDSCRIPT} master ${INSTALLROOT}/${DXVK_INSTALLATION} --no-package
-
-        echo "Tar DXVK"
-        pushd ${INSTALLROOT}
-        tar -czf ${DXVK_INSTALLATION}.tar.gz ${DXVK_INSTALLATION}
-        popd
-
-        echo "Upload DXVK"
-        mkdir -p ${PACKAGE_UPLOAD}
-        cp ${INSTALLROOT}/${DXVK_INSTALLATION}.tar.gz ${PACKAGE_UPLOAD}/
-    fi
-fi
-
 ############ Build 64bit Version ##############
 
 echo "Configure wine64-${CROSS_OVER_VERSION}"
 export CC=clang
 export CXX=clang++
+
 # see https://github.com/Gcenx/macOS_Wine_builds/issues/17#issuecomment-750346843
 export CROSSCFLAGS=$([[ ${CROSS_OVER_VERSION} -le 20.0.2 ]] && echo "-g -O2 -fcommon" || echo "-g -O2")
+
 # Xcode12 by default enables '-Werror,-Wimplicit-function-declaration' (49917738)
 # this causes wine(64) builds to fail so needs to be disabled.
 # https://developer.apple.com/documentation/xcode-release-notes/xcode-12-release-notes
@@ -186,8 +154,10 @@ popd
 echo "Configure wine32on64-${CROSS_OVER_VERSION}"
 export CC=clang
 export CXX=clang++
+
 # see https://github.com/Gcenx/macOS_Wine_builds/issues/17#issuecomment-750346843
 export CROSSCFLAGS=$([[ ${CROSS_OVER_VERSION} -le 20.0.2 ]] && echo "-g -O2 -fcommon" || echo "-g -O2")
+
 # Xcode12 by default enables '-Werror,-Wimplicit-function-declaration' (49917738)
 # this causes wine(64) builds to fail so needs to be disabled.
 # https://developer.apple.com/documentation/xcode-release-notes/xcode-12-release-notes
