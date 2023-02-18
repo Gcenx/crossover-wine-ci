4d445c196f46cd1021e76fe5b933616c976760b4
Dean M Greer <38226388+Gcenx@users.noreply.github.com>
2023-02-18 11:02:18 -0500

    Update build_local.sh

--- build_local.sh
+++ build_local.sh
@@ -77,6 +77,9 @@ export LDFLAGS="-Wl,-headerpad_max_install_names -Wl,-rpath,/opt/local/lib"
 # avoid weird linker errors with Xcode 10 and later
 export MACOSX_DEPLOYMENT_TARGET=10.14
 
+export PKG_CONFIG_DIR=""
+export PKG_CONFIG_LIBDIR="$(brew --prefix)/lib/pkgconfig:$(brew --prefix)/share/pkgconfig"
+
 export ac_cv_lib_soname_MoltenVK="libMoltenVK.dylib"
 export ac_cv_lib_soname_vulkan=""
 
