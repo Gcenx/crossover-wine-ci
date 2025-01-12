6678c30f2e62e6baf088c90fe93dd233e382765c
Dean M Greer <38226388+Gcenx@users.noreply.github.com>
2025-01-11 21:23:43 -0500

    Update build_with_macports.sh

--- build_with_macports.sh
+++ build_with_macports.sh
@@ -17,6 +17,7 @@ endgroup() {
 
 export GITHUB_WORKSPACE=$(pwd)
 
+# Only suports building 23.0.0 or later
 if [ -z "$CROSS_OVER_VERSION" ]; then
     export CROSS_OVER_VERSION=23.7.1
     echo "CROSS_OVER_VERSION not set building crossover-wine-${CROSS_OVER_VERSION}"
